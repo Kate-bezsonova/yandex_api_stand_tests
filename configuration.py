33c5d0684b28d2572482f2fb265b72d3dcdd2c4e
Gvendelin <kate-bezsonova@mail.ru>
2023-10-31 00:00:18 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://bc3d3579-7b78-4f6a-a49a-6728ddb8ebef.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/" #ручка логов сервера
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits"
